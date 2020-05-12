4ed5e07cc74c917cf985af49b7a2e2795c8b0b88
yuqiu-pp <41912446@qq.com>
2020-05-13 07:06:15 +0800

    update

---
+++ src/leetcode/editor/cn/[515]在每个树行中找最大值.java
@@ -0,0 +1,46 @@
+//您需要在二叉树的每一行中找到最大的值。 
+//
+// 示例： 
+//
+// 
+//输入: 
+//
+//          1
+//         / \
+//        3   2
+//       / \   \  
+//      5   3   9 
+//
+//输出: [1, 3, 9]
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+package leetcode.editor.cn;
+
+import java.util.List;
+
+class LC515{
+    public static void main(String[] args) {
+        Solution solution = new LC515().new Solution();
+        // TO TEST
+        System.out.println(solution.largestValues());
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
